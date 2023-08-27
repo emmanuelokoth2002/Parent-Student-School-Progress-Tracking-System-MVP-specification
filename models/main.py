b856a70e8812d71cdd4321e9b1aedd92707d5291
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-08-27 21:34:34 +0300

    Updates

--- models/main.py
+++ models/main.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from users import users_bp
+from students import students_bp
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.register_blueprint(users_bp)
+app.register_blueprint(students_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
