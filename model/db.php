7a3efce7976818f537b1310cba80271bfb7ace89
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2024-02-07 14:29:17 +0300

    database file

---
+++ model/db.php
@@ -0,0 +1,50 @@
+<?php 
+    session_start();
+    # remove after implementing login
+    if(!isset($_SESSION['userid'])){
+        $_SESSION['userid']=1;
+    }
+
+    $sql='';
+
+    class db{
+        private $servername;
+        private $username;
+        private $password;
+        private $dbname;
+        private $charset;
+              
+        public function connect(){
+            $this->servername="localhost";
+            $this->username="root";
+            $this->password="";
+            $this->charset="utf8mb4";
+            $this->dbname='contacts'; 
+           
+            try{
+                // check if user is logged in otherwise redirect to the login page
+
+                $dsn="mysql:host=".$this->servername.";dbname=".$this->dbname.";charset=".$this->charset;
+                $pdo=new PDO($dsn,$this->username,$this->password);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+                return $pdo;
+            }catch(PDOException $e){
+                echo "Connection failed: ".$e->getMessage();
+            }
+        }
+
+        public function mySQLDate($date){
+            $date = DateTime::createFromFormat('d-M-Y', $date);
+            return $date->format('Y-m-d');
+        }
+
+        public function getData($sql){
+            return $this->connect()->query($sql);
+        }
+
+        public function getJSON($sql){
+            $rst=$this->getData($sql);
+            return json_encode($rst->fetchAll(PDO::FETCH_ASSOC));
+   }
+    }
+?>
